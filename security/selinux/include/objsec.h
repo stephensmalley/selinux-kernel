9a90a4c6069190517c15a6bbad024070b16e4cb0
Stephen Smalley <stephen.smalley.work@gmail.com>
2020-08-21 16:07:27 -0400

    selinux: support per-task/cred selinux namespace
    Extend the task security structure to include a reference to
    the associated selinux namespace, and to also contain a
    pointer to the cred in the parent namespace.  The current selinux
    namespace is changed to the per-task/cred selinux namespace
    for the current task/cred.
    
    This change makes it possible to support per-cred selinux namespaces,
    but does not yet introduce a mechanism for unsharing of the selinux
    namespace.  Thus, by itself, this change does not alter the existing
    situation with respect to all processes still using a single init
    selinux namespace.
    
    An alternative would be to hang the selinux namespace off of the
    user namespace, which itself is associated with the cred.  This
    seems undesirable however since DAC and MAC are orthogonal, and
    there appear to be real use cases where one will want to use selinux
    namespaces without user namespaces and vice versa. However, one
    advantage of hanging off the user namespace would be that it is already
    associated with other namespaces, such as the network namespace, thus
    potentially facilitating looking up the relevant selinux namespace from
    the network input/forward hooks.  In most cases however, it appears that
    we could instead copy a reference to the creating task selinux namespace
    to sock security structures and use that in those hooks.
    
    Introduce a task_security() helper to obtain the correct task/cred
    security structure from the hooks, and update the hooks to use it.
    This returns a pointer to the security structure for the task in
    the same selinux namespace as the caller, or if there is none, a
    fake security structure with the well-defined unlabeled SIDs.  This
    ensures that we return a valid result that can be used for permission
    checks and for returning contexts from e.g. reading /proc/pid/attr files.
    
    Signed-off-by: Stephen Smalley <stephen.smalley.work@gmail.com>

--- security/selinux/include/objsec.h
+++ security/selinux/include/objsec.h
@@ -29,35 +29,11 @@
 #include "flask.h"
 #include "avc.h"
 
-struct avdc_entry {
-	u32 isid; /* inode SID */
-	u32 allowed; /* allowed permission bitmask */
-	u32 audited; /* audited permission bitmask */
-	bool permissive; /* AVC permissive flag */
-};
-
-struct task_security_struct {
-	u32 osid; /* SID prior to last execve */
-	u32 sid; /* current SID */
-	u32 exec_sid; /* exec SID */
-	u32 create_sid; /* fscreate SID */
-	u32 keycreate_sid; /* keycreate SID */
-	u32 sockcreate_sid; /* fscreate SID */
-#define TSEC_AVDC_DIR_SIZE (1 << 2)
-	struct {
-		u32 sid; /* current SID for cached entries */
-		u32 seqno; /* AVC sequence number */
-		unsigned int dir_spot; /* dir cache index to check first */
-		struct avdc_entry dir[TSEC_AVDC_DIR_SIZE]; /* dir entries */
-		bool permissive_neveraudit; /* permissive and neveraudit */
-	} avdcache;
-} __randomize_layout;
-
 static inline bool task_avdcache_permnoaudit(struct task_security_struct *tsec)
 {
 	return (tsec->avdcache.permissive_neveraudit &&
 		tsec->sid == tsec->avdcache.sid &&
-		tsec->avdcache.seqno == avc_policy_seqno());
+		tsec->avdcache.seqno == avc_policy_seqno(tsec->state));
 }
 
 enum label_initialized {
@@ -171,10 +147,6 @@ struct perf_event_security_struct {
 };
 
 extern struct lsm_blob_sizes selinux_blob_sizes;
-static inline struct task_security_struct *selinux_cred(const struct cred *cred)
-{
-	return cred->security + selinux_blob_sizes.lbs_cred;
-}
 
 static inline struct file_security_struct *selinux_file(const struct file *file)
 {
@@ -199,16 +171,6 @@ static inline struct ipc_security_struct *
 selinux_ipc(const struct kern_ipc_perm *ipc)
 {
 	return ipc->security + selinux_blob_sizes.lbs_ipc;
-}
-
-/*
- * get the subjective security ID of the current task
- */
-static inline u32 current_sid(void)
-{
-	const struct task_security_struct *tsec = selinux_cred(current_cred());
-
-	return tsec->sid;
 }
 
 static inline struct superblock_security_struct *
