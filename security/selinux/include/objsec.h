b86d7e185fd91f9141f2f19bd38f1932a6dc309f
Stephen Smalley <stephen.smalley.work@gmail.com>
2024-07-30 11:42:54 -0400

    selinux: restore passing of selinux_state
    This reverts commit e67b79850fcc4eb5 ("selinux: stop passing selinux_state
    pointers and their offspring"). This change is necessary in order to
    support SELinux namespaces.
    
    Signed-off-by: Stephen Smalley <stephen.smalley.work@gmail.com>

--- security/selinux/include/objsec.h
+++ security/selinux/include/objsec.h
@@ -57,7 +57,7 @@ static inline bool task_avdcache_permnoaudit(struct task_security_struct *tsec)
 {
 	return (tsec->avdcache.permissive_neveraudit &&
 		tsec->sid == tsec->avdcache.sid &&
-		tsec->avdcache.seqno == avc_policy_seqno());
+		tsec->avdcache.seqno == avc_policy_seqno(&selinux_state));
 }
 
 enum label_initialized {
