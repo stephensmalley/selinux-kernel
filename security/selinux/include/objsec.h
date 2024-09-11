5875b23b870472fae6b51acaf53e0dadb2c28602
Stephen Smalley <stephen.smalley.work@gmail.com>
2024-09-11 14:16:09 -0400

    selinux: update hook functions to use correct selinux namespace
    Update the SELinux hook functions to use the correct SELinux namespace
    rather than always using the current SELinux namespace. This is
    necessary for hook functions that can be called outside of process
    context. In order to have a SELinux namespace available for use in all
    such hook functions, this change saves a reference to the current
    SELinux namespace in open file security blobs for use by send_sigiotask
    and in sock security blobs for use in network input or forward hooks. It
    also fixes a few remaining uses of current_selinux_state that should be
    using cred_selinux_state() on an appropriate cred structure.
    
    In order to correctly label nodes and network interfaces, the SELinux
    namespace is passed from the hook functions to the sel_netnode and
    sel_netif interfaces and cached along with the other information.
    Similarly, where needed, the SELinux namespace is passed from the hook
    functions to the selinux_netlbl and selinux_xfrm interfaces for use in
    invoking various security server or AVC interfaces.
    
    There are a few residual xfrm functions where we do not have a
    SELinux namespace available currently. For now these functions
    always use the init SELinux namespace.
    
    Signed-off-by: Stephen Smalley <stephen.smalley.work@gmail.com>

--- security/selinux/include/objsec.h
+++ security/selinux/include/objsec.h
@@ -28,6 +28,7 @@
 #include <net/net_namespace.h>
 #include "flask.h"
 #include "avc.h"
+#include "security.h"
 
 static inline bool task_avdcache_permnoaudit(struct task_security_struct *tsec)
 {
@@ -58,6 +59,7 @@ struct file_security_struct {
 	u32 fown_sid; /* SID of file owner (for SIGIO) */
 	u32 isid; /* SID of inode at the time of file open */
 	u32 pseqno; /* Policy seqno at the time of file open */
+	struct selinux_state *state; /* SELinux state */
 };
 
 struct superblock_security_struct {
@@ -84,6 +86,7 @@ struct netif_security_struct {
 	const struct net *ns; /* network namespace */
 	int ifindex; /* device index */
 	u32 sid; /* SID for this interface */
+	struct selinux_state *state; /* SELinux state */
 };
 
 struct netnode_security_struct {
@@ -93,6 +96,7 @@ struct netnode_security_struct {
 	} addr;
 	u32 sid; /* SID for this node */
 	u16 family; /* address family */
+	struct selinux_state *state; /* SELinux state */
 };
 
 struct netport_security_struct {
@@ -119,6 +123,7 @@ struct sk_security_struct {
 	       SCTP_ASSOC_UNSET = 0,
 	       SCTP_ASSOC_SET,
 	} sctp_assoc_state;
+	struct selinux_state *state; /* SELinux state */
 };
 
 struct tun_security_struct {
