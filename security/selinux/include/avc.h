d54504630846731da41c60854a2779f9623d12ff
Stephen Smalley <stephen.smalley.work@gmail.com>
2024-09-30 12:52:31 -0400

    selinux: add kerneldoc to new permission checking functions
    Add kerneldoc to new namespace-aware permission checking
    functions.
    
    Signed-off-by: Stephen Smalley <stephen.smalley.work@gmail.com>

--- security/selinux/include/avc.h
+++ security/selinux/include/avc.h
@@ -148,7 +148,7 @@ int avc_has_extended_perms(struct selinux_state *state, u32 ssid, u32 tsid,
 
 int cred_task_has_perm(const struct cred *cred, const struct task_struct *p,
 		       u16 tclass, u32 requested,
-		       struct common_audit_data *auditdata);
+		       struct common_audit_data *ad);
 
 int cred_has_extended_perms(const struct cred *cred, u32 tsid, u16 tclass,
 			    u32 requested, u8 driver, u8 base_perm, u8 xperm,
